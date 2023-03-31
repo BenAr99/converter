07131d448e9d2a31427c01d2075d293fbb7c1688
Suleiman_Kuliev <getto_tawers@mail.ru>
2023-03-31 18:18:02 +0400

    исправил средние по уровню ошибки

--- convertor.js
+++ convertor.js
@@ -1,7 +1,3 @@
-// java -jar "C:\Users\getto\Downloads\Telegram Desktop\currencyconverter-0.0.1-SNAPSHOT.jar"
-// cd C:\Program Files\Google\Chrome\Application
-// chrome.exe --user-data-dir="C://chrome-dev-disabled-security" --disable-web-security --disable-site-isolation-trials
-// авторизация
 function handleAuthClick(event) {
     if (event.target.id === 'log-in') {
         showAuthorizationModal()
@@ -21,9 +17,9 @@ function showAuthorizationModal() {
         <span>Email</span> <input type="text">
         <span>Password</span> <input type="password"></div>
         <button class="modal-authorization-button modal-authorization-logIn-button">Log In</button>
-        <button class="modal-authorization-button google-logo"><img src="assets/icons/google-logo.svg">Log in with Google </button>
-        <button class="modal-authorization-button github-logo"><img src="assets/icons/github_logo_icon_147285.svg">Log in with GitHub </button>
-        <button class="modal-authorization-button facebook-logo"><img src="assets/icons/facebook-logo.svg">Log in with Facebook </button>
+        <button class="modal-authorization-button google-logo"><img alt="" src="assets/icons/google-logo.svg">Log in with Google </button>
+        <button class="modal-authorization-button github-logo"><img alt="" src="assets/icons/github_logo_icon_147285.svg">Log in with GitHub </button>
+        <button class="modal-authorization-button facebook-logo"><img alt="" src="assets/icons/facebook-logo.svg">Log in with Facebook </button>
         </div>`
     authorizationModal.className = 'authorization-modal-container';
     document.querySelector('body').prepend(authorizationModal);
@@ -42,10 +38,10 @@ document.querySelector('.authorization').onclick = handleAuthClick;
 
 
 function swapPlacesValue() {
-    let firstValueInput = document.getElementById('output_value_first').value;
-    let secondValueInput = document.getElementById('output_value_second').value;
-    document.getElementById('output_value_first').value = secondValueInput
-    document.getElementById('output_value_second').value = firstValueInput
+    let fromValueInput = document.getElementById('output_value_in').value;
+    let inValueInput = document.getElementById('output_value_from').value;
+    document.getElementById('output_value_in').value = fromValueInput
+    document.getElementById('output_value_from').value = inValueInput
 }
 
 document.querySelector('.swap-places_img').onclick = swapPlacesValue
@@ -83,7 +79,7 @@ async function getCurrency() {
     }
 }
 
-function idPostRequestFirst(body, idBaseCurrency, idConversionCurrency) {
+function idPostRequestFrom(body, idBaseCurrency, idConversionCurrency) {
     getCurrency().then(data => {
         for (let i = 0; i < data.currencies.length; i++) {
             if (data.currencies[i].charCode.includes(idBaseCurrency)) {
@@ -96,12 +92,12 @@ function idPostRequestFirst(body, idBaseCurrency, idConversionCurrency) {
         }
         postCurrency(body, idBaseCurrency, idConversionCurrency).then(data => {
             console.log(data)
-            document.getElementById('output_value_second').value = data.exchangeResultAmount
+            document.getElementById('output_value_from').value = data.exchangeResultAmount
         })
     })
 }
 
-function idPostRequestSecond(body, idBaseCurrency, idConversionCurrency) {
+function idPostRequestIn(body, idBaseCurrency, idConversionCurrency) {
     getCurrency().then(data => {
         for (let i = 0; i < data.currencies.length; i++) {
             if (data.currencies[i].charCode.includes(idBaseCurrency)) {
@@ -114,46 +110,46 @@ function idPostRequestSecond(body, idBaseCurrency, idConversionCurrency) {
         }
         postCurrency(body, idBaseCurrency, idConversionCurrency).then(data => {
             console.log(data)
-            document.getElementById('output_value_first').value = data.exchangeResultAmount
+            document.getElementById('output_value_in').value = data.exchangeResultAmount
         })
     })
 }
 
 
-function doConverisonOutputFirst(event) {
-    const inputValue = document.getElementById('output_value_first').value;
-    const idBaseCurrency = document.getElementById('currency-selection-first').value.slice(0, 3)
-    const idConversionCurrency = document.getElementById('currency-selection-second').value.slice(0, 3)
+function doConverisonOutputfrom() {
+    const inputValue = document.getElementById('output_value_in').value;
+    const idBaseCurrency = document.getElementById('currency-selection-in').value.slice(0, 3)
+    const idConversionCurrency = document.getElementById('currency-selection-from').value.slice(0, 3)
     const dataBoxInput = {
         amount: inputValue,
     }
-    idPostRequestFirst(dataBoxInput, idBaseCurrency, idConversionCurrency)
+    idPostRequestFrom(dataBoxInput, idBaseCurrency, idConversionCurrency)
 }
 
-function doConversionOutputSecond() {
-    const inputValue = document.getElementById('output_value_second').value;
-    const idBaseCurrency = document.getElementById('currency-selection-second').value.slice(0, 3)
-    const idConversionCurrency = document.getElementById('currency-selection-first').value.slice(0, 3)
+function doConversionOutputfrom() {
+    const inputValue = document.getElementById('output_value_from').value;
+    const idBaseCurrency = document.getElementById('currency-selection-from').value.slice(0, 3)
+    const idConversionCurrency = document.getElementById('currency-selection-in').value.slice(0, 3)
     const dataBoxInput = {
         amount: inputValue,
     }
-    idPostRequestSecond(dataBoxInput, idBaseCurrency, idConversionCurrency)
+    idPostRequestIn(dataBoxInput, idBaseCurrency, idConversionCurrency)
 }
 
 // бд
 
 function сurrencyСonversion() {
-    let firstInputConversion = document.getElementById('output_value_first')
-    let secondInputConversion = document.getElementById('output_value_second')
+    let fromInputConversion = document.getElementById('output_value_in')
+    let inInputConversion = document.getElementById('output_value_from')
 
 
-    let firstSelectConversion = document.getElementById('currency-selection-first')
-    let secondSelectConversion = document.getElementById('currency-selection-second')
-    firstInputConversion.oninput = doConverisonOutputFirst
-    secondInputConversion.oninput = doConversionOutputSecond
+    let fromSelectConversion = document.getElementById('currency-selection-in')
+    let inSelectConversion = document.getElementById('currency-selection-from')
+    fromInputConversion.oninput = doConverisonOutputfrom
+    inInputConversion.oninput = doConversionOutputfrom
 
-    firstSelectConversion.oninput = doConverisonOutputFirst
-    secondSelectConversion.oninput = doConversionOutputSecond
+    fromSelectConversion.oninput = doConverisonOutputfrom
+    inSelectConversion.oninput = doConversionOutputfrom
 }
 
 сurrencyСonversion()
@@ -192,15 +188,11 @@ getCurrency().then(data => {
     document.getElementById('code-search').onkeyup = searchTable // focus?
     document.getElementById('currency-search').onkeyup = searchTable
 
-    // Выбор валюты в вкладыше
-    function currencySelection() {
-        document.querySelectorAll('.currency-selection').forEach(item => {
-            for (let i = 0; data.currencies.length > i; i++) {
-                item.innerHTML += `<option>${data.currencies[i].charCode} - ${data.currencies[i].name}</option>`
-            }
-        })
-    }
-
-    currencySelection()
+    // Cоздание валюты в вкладыше
+    document.querySelectorAll('.currency-selection').forEach(item => {
+        for (let i = 0; data.currencies.length > i; i++) {
+            item.innerHTML += `<option>${data.currencies[i].charCode} - ${data.currencies[i].name}</option>`
+        }
+    })
 })
 
